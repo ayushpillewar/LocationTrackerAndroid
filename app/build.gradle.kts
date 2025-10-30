dbba9eb903b1d5caf07f2fc2399657da8c2bfe6c
aayus <aayushpllwr@gmail.com>
2025-10-30 13:15:07 +0530

    verison change

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.majboormajdoor.locationtracker"
         minSdk = 24
         targetSdk = 35
-        versionCode = 5
-        versionName = "5.0"
+        versionCode = 6
+        versionName = "6.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
